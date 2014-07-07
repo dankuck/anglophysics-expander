c36d90a1a6577380a8043b16251e730092a452fa
dankuck <dankuck@gmail.com>
2014-07-07 17:04:46 -0400

    Supposed to check for a file just named "nouns", not the source file "nouns.txt"

--- check_build_dict.php
+++ check_build_dict.php
@@ -2,5 +2,5 @@
 
 require_once dirname(__FILE__) . '/bootstrappy.php';
 
-if (! file_exists(APP_ROOT . '/dict/nouns.txt'))
+if (! file_exists(APP_ROOT . '/dict/nouns'))
 	require_once APP_ROOT . '/build_dict.php';
