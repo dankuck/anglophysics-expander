424e9fd0f1d56f97e6e10ced3695859088387be0
dankuck <dankuck@gmail.com>
2014-07-07 03:55:17 -0400

    How to permutate words together orderlessly without repeats.
    Also fix a little something in that other permutator.

--- classes/AnglophysicsExpander.php
+++ classes/AnglophysicsExpander.php
@@ -18,9 +18,9 @@ class AnglophysicsExpander{
 
 	public function step(){
 		$words = AnglophysicsExpander_WordGenerator::generate_potential_words($this->words, $this->max_word_length);
-		$words = AnglophysicsExpander_ArrayRemainder::eliminate($words, $this->words);
+		$words = AnglophysicsExpander_ArrayRemainder::eliminate($words, $this->words); // we don't have to do this now, but it could speed up the following steps
 		$words = AnglophysicsExpander_WordEliminator::eliminate_non_words($words);
-		$words = AnglophysicsExpander_CombinationFinder::eliminate_uncombinable_words(array_merge($this->words, $words));
+		$words = AnglophysicsExpander_CombinationFinder::eliminate_uncombinable_words($this->words, $words);
 		$words = AnglophysicsExpander_ArrayRemainder::eliminate($words, $this->words);
 		$this->words_by_phase[] = $words;
 		$this->words = array_merge($this->words, $words);
@@ -60,7 +60,7 @@ class AnglophysicsExpander_WordGenerator{
 	public static function generate_potential_words($words, $max_length = 8){
 		$letters = self::letters_from($words);
 		$gen = array();
-		$it = new AnglophysicsExpander_WordGenerator_Iterator($letters, $max_length);
+		$it = new AnglophysicsExpander_WordGenerator_Permutator($letters, $max_length);
 		while (! $it->done()){
 			$word = $it->next();
 			if (self::nothing_starts_with($word)){
@@ -92,13 +92,14 @@ class AnglophysicsExpander_WordGenerator{
 	}
 }
 
-class AnglophysicsExpander_WordGenerator_Iterator{
+class AnglophysicsExpander_WordGenerator_Permutator{
 
 	public function __construct($letters, $max_length = 8){
 		$this->letters = array_merge(array(''), $letters);
 		$this->current = array();
-		for ($i = 0; $i < $max_length; $i++)
-			$this->current[] = 0;
+		for ($i = 0; $i < $max_length; $i++){
+			$this->current[$i] = 0;
+		}
 		$this->increment();
 	}
 
@@ -130,7 +131,8 @@ class AnglophysicsExpander_WordGenerator_Iterator{
 			$this->current[$i]++;
 			if ($this->current[$i] < count($this->letters))
 				return; // increment done.
-			$this->current[$i] = 0; // increment this to 0 and go on to increment the next highest one.
+			// set this to 0 and go on to increment the next highest one.
+			$this->current[$i] = 0; 
 		}
 		$this->done = true; // if we haven't already returned, then we got all the way to the beginning of current without anything valid. We're done.
 	}
@@ -171,7 +173,110 @@ class AnglophysicsExpander_ArrayRemainder{
 
 class AnglophysicsExpander_CombinationFinder{
 
-	public static function eliminate_uncombinable_words($words){
+	public static function eliminate_uncombinable_words($start, $words){
+		$letter_groups = self::letter_groups($start);
 		return $words;
 	}
+
+	public static function letter_groups($start){
+		$it = new AnglophysicsExpander_WordGenerator_Permutator($start, count($start));
+		$groups = array();
+		while (! $it->done()){
+			$letters = array_unique(preg_split('//', $it->next()));
+			sort($letters);
+			$groups[join('', $letters)] = $letters;
+		}
+		return $groups;
+	}
 }
+
+/**
+ * Every unique order-less combination of words should come up once.
+ * More than once is acceptable but wasteful.
+ *
+ * This works by keeping an array of queues.
+ * Everytime increment() is called, it shifts the top item off of the last queue
+ * If that queue is now too short for the following copy operation, it jumps up to the next-to-last queue (etc)
+ * Once a queue shifts and remains long enough for this operation:
+ * It copies the queue down to the next queue and shifts that one's top, 
+ * And then repeats until the last queue is reached again.
+ * If the last isn't reached, it's done.
+ * If the first went empty it's done.
+ *
+ * So it looks like this:
+ *
+ * Initialization with array('X', 'Y', 'Z'), length=2:
+ *
+ * Array1 Array2
+ * X      Y
+ * Y      Z
+ * Z
+ *
+ * INCREMENT:
+ *
+ * Array1 Array2
+ * X      Z
+ * Y      
+ * Z
+ *
+ * INCREMENT: 
+ * 
+ * Array1 Array2
+ * Y      Z
+ * Z
+ *
+ * INCREMENT:
+ *
+ * DONE.
+ * 
+ *
+ */
+class AnglophysicsExpander_CombinationFinder_Permutator{
+
+	public function __construct($words, $length){
+		$this->words = array_merge(array(''), $words);
+		$this->current = array();
+		$this->current[0] = array_merge(array(array()), $words);
+		for ($i = 1; $i < $length; $i++)
+			$this->current[$i] = array();
+		$this->increment();
+	}
+
+	public function done(){
+		return $this->done;
+	}
+
+	public function next(){
+		if ($this->done)
+			return;
+		$next = array();
+		foreach ($this->current as $i){
+			$next[] = $i[0];
+		}
+		$this->increment();
+		return $next;
+	}
+
+	public function increment(){
+		if ($this->done)
+			return;
+		for ($i = count($this->current) - 1; $i >= 0; $i--){
+			array_shift($this->current[$i]);
+			if (count($this->current[$i]) < count($this->current) - $i)
+				continue; // go on to increment the next highest one.
+			for ($i++; $i < count($this->current); $i++){
+				$this->current[$i] = $this->current[$i - 1];
+				array_shift($this->current[$i]);
+				if (! $this->current[$i]){
+					$this->done = true;
+					return;
+				}
+			}
+			return; // increment done.
+		}
+		$this->done = true; // if we haven't already returned, then we got all the way to the beginning of current without anything valid. We're done.
+	}
+
+}
+
+
