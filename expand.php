48adbdc3ae60c0f88340acc57be4084c44ffa480
dankuck <dankuck@gmail.com>
2014-07-08 12:27:08 -0400

    Doesn't take too long or much memory to generate 7 letter words. Lets see how it affects the rest of the system.

--- expand.php
+++ expand.php
@@ -8,7 +8,7 @@ else{
 	// figure out php://input
 }
 
-$exp = new AnglophysicsExpander($base_words, 4);
+$exp = new AnglophysicsExpander($base_words, 7);
 $echo = new ConsoleEcho($exp);
 $exp->set_callbacks(array(
 	step => array($echo, 'stepped'),
